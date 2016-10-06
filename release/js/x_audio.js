20f46bb422f3db287e979e3f12e501bd3866b08a
XCVG <xcvgvcx@gmail.com>
2016-10-05 19:09:21 -0700

    removed the audio stuff that didn't work

--- release/js/x_audio.js
+++ release/js/x_audio.js
@@ -29,8 +29,8 @@
 	if(_x_audio_music)
 		_x_audio_music.pause();
 	
-	//_x_audio_music = new Audio(song_path);
-	_x_audio_music = x_audio_loadAudio(music);
+	_x_audio_music = new Audio(song_path);
+	//_x_audio_music = x_audio_loadAudio(music);
 	_x_audio_music.loop = true;
 	_x_audio_music.load();
 	_x_audio_music.play();
@@ -47,62 +47,6 @@
 		_x_audio_music = null;
 	}
  }
- 
- //load an audio file into an Audio object and return
- //soundType can be sounds, music, path, or both
- function x_audio_loadAudio(soundName, soundType="path")
- {
-	 var soundPath;
-	 var audio;
-	 
-	 if(soundType === "sounds")
-	 {
-		 //look in sounds folder
-		 audio = _x_audio_loadAudioFromPath("sounds/" + soundName);
-	 }
-	 else if(soundType === "music")
-	 {
-		 //look in music folder
-		 audio = _x_audio_loadAudioFromPath("music/" + soundName);
-	 }
-	 else if(soundType === "path")
-	 {
-		 //treat soundName as a full path
-		 audio = _x_audio_loadAudioFromPath(soundName);
-	 }
-	 else if(soundType === "both")
-	 {
-		//try music, then sounds
-		audio = _x_audio_loadAudioFromPath("music/" + soundName);
-		if(!audio)
-			audio = _x_audio_loadAudioFromPath("sounds/" + soundName);
-	 }
-	 else
-	 {
-		 console.log("Unknown sound path type " + soundType);
-	 }
-	 
-	 if(!audio)
-		 console.log("Failed to load " + soundName);
-	 
-	 return audio;
- }
- 
- //internal function tries to load audio from path
- //return null on failure
- function _x_audio_loadAudioFromPath(soundPath)
- {
-	 var audio;
-	 //try WAV, then OGG, then MP3
-	 audio = new Audio(soundPath + ".wav");
-	 audio.load();
-	 console.log(audio.error);
-	 if(!audio)
-		 audio = new Audio(soundPath + ".ogg");
-	 if(!audio)
-		 audio = new Audio(soundPath + ".mp3");
-	 
-	 return audio;
-	 
- }
+
+
  

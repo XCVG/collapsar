07c67f232c803144d42d0f18b9438790a48f4d25
XCVG <xcvgvcx@gmail.com>
2016-10-21 17:15:15 -0700

    oops

---
+++ release/data/soundinfo.js
@@ -0,0 +1,96 @@
+//ugly but the only way to make it work synchronously and across platforms
+var data_soundinfo =
+{
+  "sounds": {
+  "attack": {
+    "src": "sounds/attack.wav"
+  },
+  "miss": {
+    "src": "sounds/miss.wav"
+  },
+  "critical": {
+    "src": "sounds/critical.wav"
+  },
+  "heal": {
+    "src": "sounds/heal.wav"
+  },
+  "fire": {
+    "src": "sounds/fire.wav"
+  },
+  "coin": {
+    "src": "sounds/coin.wav"
+  },
+  "hpdrain": {
+    "src": "sounds/hpdrain.wav"
+  },
+  "mpdrain": {
+    "src": "sounds/mpdrain.wav"
+  },
+  "run": {
+    "src": "sounds/run.wav"
+  },
+  "blocked": {
+    "src": "sounds/blocked.wav"
+  },
+  "defeat": {
+    "src": "sounds/defeat.wav"
+  },
+  "boneshield": {
+    "src": "sounds/boneshield.wav"
+  },
+  "click": {
+    "src": "sounds/click.wav"
+  },
+  "unlock": {
+    "src": "sounds/unlock.wav"
+  },
+  "0": {
+    "alias": "attack"
+  },
+  "1": {
+    "alias": "miss"
+  },
+  "2": {
+    "alias": "critical"
+  },
+  "3": {
+    "alias": "heal"
+  },
+  "4": {
+    "alias": "fire"
+  },
+  "5": {
+    "alias": "coin"
+  },
+  "6": {
+    "alias": "hpdrain"
+  },
+  "7": {
+    "alias": "mpdrain"
+  },
+  "8": {
+    "alias": "run"
+  },
+  "9": {
+    "alias": "blocked"
+  },
+  "10": {
+    "alias": "defeat"
+  },
+  "11": {
+    "alias": "boneshield"
+  },
+  "12": {
+    "alias": "click"
+  },
+  "13": {
+    "alias": "unlock"
+  }
+},
+  "music": {
+      "intro": {
+    "src": "music/d_dm2ttl.ogg"
+   }      
+  }
+}
+;

e3395195454a9fdb09628438eeef0a4066c6cd80
XCVG <xcvgvcx@gmail.com>
2017-04-06 14:06:53 -0700

    added intro and shield

--- release/js/ending2.js
+++ release/js/ending2.js
@@ -11,12 +11,16 @@ ending2.bg = new Image();
 ending2.bg_loaded = false;
 ending2.fg = new Image();
 ending2.fg_loaded = false;
+ending2.fg2 = new Image();
+ending2.fg2_loaded = false;
 ending2.pos = -60; //a hack
 ending2.posEnd = 440; //remember, screen coords are 160x120
+ending2.posKill = 800;
 ending2.delayCounter = 0;
 ending2.scrollDelay = 0;
 ending2.scrollSpeed = 0.1;
 ending2.transparencyFactor = 0.05;
+ending2.drawFG2 = false;
 
 function ending2_init()
 {
@@ -24,11 +28,14 @@ function ending2_init()
   ending2.bg.onload = function() {ending2_bg_onload();};
   ending2.fg.src = "images/backgrounds/ending_fg.png";
   ending2.fg.onload = function() {ending2_fg_onload();};
+  ending2.fg2.src = "images/backgrounds/ending_fg2.png";
+  ending2.fg2.onload = function() {ending2_fg2_onload();};
   redraw = true;
 }
 
 function ending2_bg_onload() {ending2.bg_loaded = true;}
 function ending2_fg_onload() {ending2.fg_loaded = true;}
+function ending2_fg2_onload() {ending2.fg2_loaded = true;}
 
 
 function ending2_logic()
@@ -54,12 +61,25 @@ function ending2_logic()
 			ending2.delayCounter++;
 		}
 	}
-	else
+	else if(ending2.pos < ending2.posKill)
 	{
             //x_audio_stopMusic(); //because I'm too lazy to set up looping/nonlooping
 		//ending2_startGame();
                 //TODO: endgame
+                ending2.drawFG2 = true;
+                ending2.pos += ending2.scrollSpeed;
+                redraw = true;
+                
+                if ((pressing.mouse && !input_lock.mouse) || (pressing.action && !input_lock.action))
+                {  
+                        //on mouse press, go to the next scene
+                        window.location.reload();
+                }
 	}
+        else
+        {
+            window.location.reload();
+        }
 
 }
 
@@ -67,6 +87,7 @@ function ending2_render()
 {
   //console.log(ending2.pos);
   
+  //the onload in this thing is a bit broken
   if (!bitfont.loaded || !ending2.bg_loaded || !ending2.fg_loaded) {
     redraw = true;
     return;
@@ -83,8 +104,22 @@ function ending2_render()
   ctx.drawImage(ending2.bg, 0, 0, 160*SCALE, 120*SCALE);
   ctx.restore();
   
-  ctx.drawImage(ending2.fg, 0, -ending2.pos*SCALE, 160*SCALE, 120*SCALE*4);
+  if(!ending2.drawFG2)
+  {
+      ctx.drawImage(ending2.fg, 0, -ending2.pos*SCALE, 160*SCALE, 120*SCALE*4);
+  }
+  else
+  {
+      var correctedPos = ending2.pos - ending2.posEnd;
+      //TODO: fadein FG2
+      ctx.save();
+      ctx.globalAlpha = (ending2.transparencyFactor * (correctedPos));
+      ctx.drawImage(ending2.fg2, 0, 0, 160*SCALE, 120*SCALE);
+      ctx.restore();
+  }
+  
 
+  
 
 }
 
